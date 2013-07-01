4d0f8f2dd12d5d0e68d35436a31ed6fe84812d6e
gxbsst <gxbsst@gmail.com>
2013-07-01 16:26:43 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wineplus::Application.config.secret_token = '53dfde65592d701c2bede77026c6f735ebb09aa50a98074cb9a3de6816cb7b3186d3578150c932c9d1aa0f6b6ebaaba330f430a8605c2514a5a6784853b68d2c'
